dcb263d83321f4c608e33cf90dcff70cc04ed188
CodeNinja-tech <elo48858@gmail.com>
2022-10-29 22:54:25 +0100

    Added Long Data Type

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,13 @@ fun main() {
   val maxIntegerValue: Int = Int.MAX_VALUE
   val minIntegerValue: Int = Int.MIN_VALUE
 
+  val myMaxByteValue: Byte = Byte.MAX_VALUE
+  val myMinByteValue: Byte = Byte.MIN_VALUE
+
+  val myMaxShortValue: Short = Short.MAX_VALUE
+  val myMinShortValue: Short = Short.MIN_VALUE
+
   println("Integer: $integer,\nMaximum Integer Value: $maxIntegerValue,\nMinimum Integer Value: $minIntegerValue")
+  println("\nMaximum Byte Value: $myMaxByteValue,\nMinimum Byte Value: $myMinByteValue")
+  println("\nMaximum Short Value: $myMaxShortValue,\nMinimum Short Value: $myMinShortValue")
 }
