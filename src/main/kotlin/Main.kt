c7cfadac448caa921499e0ba6f7183c2b09f7b67
CodeNinja-tech <elo48858@gmail.com>
2022-10-29 19:59:39 +0100

    Completed Project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+  val integer = 15
+  val maxIntegerValue: Int = Int.MAX_VALUE
+  val minIntegerValue: Int = Int.MIN_VALUE
+
+  println("Integer: $integer,\nMaximum Integer Value: $maxIntegerValue,\nMinimum Integer Value: $minIntegerValue")
+}
